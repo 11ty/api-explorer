a7e85a9f0b12b53d13376c6e73d4b746b103f971
Zach Leatherman <zachleatherman@gmail.com>
2021-10-27 20:14:53 -0500

    Initial commit.

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: "dynamic",
+    functionsDir: "./netlify/functions/",
+    redirects: "netlify-toml-builders",
+  });
+};
