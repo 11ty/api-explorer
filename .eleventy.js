9727cbb4b17b8b05821cf0d9c37d104790c61406
Zach Leatherman <zachleatherman@gmail.com>
2021-10-28 09:14:46 -0500

    API explorer

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+
   eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
     name: "dynamic",
     functionsDir: "./netlify/functions/",
